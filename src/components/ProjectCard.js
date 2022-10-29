ed905172fdbe85be753a1c828555a91984e68c6a
Vicky Chan <nakamanoaki@gmail.com>
2022-10-29 01:37:02 -0400

    Changed display of ProjectCard to grid, now looks better when wrapping

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -7,9 +7,9 @@ const ProjectCard = ({name, tools, descript, git, live}) => {
     const twilight = useTheme();
 
     return(
-        <Box sx={{maxWidth:"800px", width:"80%", backgroundColor:twilight.lightTurquoise, margin:"30px auto", padding:"15px", borderRadius:"15px", display:"flex", flexFlow:"wrap"}}>
+        <Box sx={{maxWidth:"800px", width:"80%", backgroundColor:twilight.lightTurquoise, margin:"30px auto", padding:"15px", borderRadius:"15px", display:"grid", gridTemplateColumns:"repeat(auto-fit, minmax(350px, 1fr))"}}>
             <img src={require(`../projImages/${name}.png`)} alt={name} className="projectImage"/>
-            <Box sx={{flexGrow:1, padding:"15px", minWidth:"350px", maxWidth:"50%"}}>
+            <Box sx={{padding:"15px", minWidth:"350px"}}>
                 <Typography variant="h3" sx={{fontFamily:"monospace", color:twilight.dark}}>{name}</Typography>
                 <Box sx={{display:"flex", flexWrap:"wrap", gap:"5px"}}>
                     {tools.map((tool) => {
